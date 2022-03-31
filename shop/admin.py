df676e502c1416e70e8de056e88ef8d07284e52a
senayberhe <tsionberhe@gmail.com>
2022-03-31 16:02:20 -0700

    created views and templates

--- shop/admin.py
+++ shop/admin.py
@@ -4,7 +4,7 @@ from .models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug', ('name',)}
+    prepopulated_fields = {'slug': ('name',)}
     
     
 @admin.register(Product)
