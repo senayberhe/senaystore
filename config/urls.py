a3546c6eba5af14ef0733f72cad6162f412c3e51
senayberhe <tsionberhe@gmail.com>
2022-04-01 23:24:16 -0700

    created user app for authentication

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,7 @@ urlpatterns = i18n_patterns (
     path(_('payment/'), include('payment.urls', namespace='payment')),
     path(_('coupons/'), include('coupons.urls', namespace='coupons')),
     path('rosetta/', include('rosetta.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('shop.urls', namespace='shop')),
 )
 if settings.DEBUG:
