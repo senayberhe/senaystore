139b062054543d1ce2a0cb20c7ecc44225af169c
senay berhe <tsionberhe@gmail.com>
2022-11-29 22:42:30 -0800

    updated files

--- myshop/settings.py
+++ myshop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'senaystore.herokuapp.com']
+ALLOWED_HOSTS = ['*', 'senaystore.onrender.com']
 
 
 # Application definition
